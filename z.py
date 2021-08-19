0d4213f338b49850c4b09cf5d15775c076a47c19
FDX100 <40120613+FDX100@users.noreply.github.com>
2021-08-19 23:27:42 +0300

    Update z.py

--- z.py
+++ z.py
@@ -1,25 +1,7 @@
-
-from youtube_transcript_api import YouTubeTranscriptApi
 from flask import Flask
-import json
-from flask import Response
-
-def get_subtl(v_id):
-        
-    srt = YouTubeTranscriptApi.get_transcript(v_id)
-
-
-
-
-    return srt
-
 
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world(v_id):
-    
-    return "hi"
-    
-if __name__ == "__main__":
-    app.run(debug=False,host='0.0.0.0')    
+def hello_world():
+    return "<p>Hello, World!</p>"
